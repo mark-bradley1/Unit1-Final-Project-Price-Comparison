33924017429fd5737a902a0cd50c316f15c5952f
Mark Bradley <mbradle15@gmail.com>
2025-10-30 10:43:07 -0500

    adjusted folder locations

---
+++ Grocery-Price-Comparison/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
