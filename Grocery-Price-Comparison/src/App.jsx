27dfeabf96875c8e7963df4c916a9521cf14fb14
Mark Bradley <mbradle15@gmail.com>
2025-11-03 17:35:14 -0600

    Created basic header component

--- Grocery-Price-Comparison/src/App.jsx
+++ Grocery-Price-Comparison/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-     
+     <Header />
     </>
   )
 }
