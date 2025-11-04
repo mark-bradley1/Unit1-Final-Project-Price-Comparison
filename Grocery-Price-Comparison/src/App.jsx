1b87e23abef6684f5dd62908cc2f2b42ca6d5733
Mark Bradley <mbradle15@gmail.com>
2025-11-03 22:38:34 -0600

    In wrong branch but added my data list for the comparison tool

--- Grocery-Price-Comparison/src/App.jsx
+++ Grocery-Price-Comparison/src/App.jsx
@@ -7,6 +7,7 @@ import AboutPage from './components/pages/AboutPage'
 import ContactPage from './components/pages/ContactPage'
 import ComparisonPage from './components/pages/ComparisonPage'
 import CartPage from './components/pages/CartPage'
+import HomePage from './components/pages/HomePage'
 
 function App() {
 
@@ -14,7 +15,7 @@ function App() {
     <>
      <Header />
      <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/cart" element={<CartPage />} />
