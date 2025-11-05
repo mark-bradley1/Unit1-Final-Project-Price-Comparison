637dfaa1bab7e5284ece5db749db1baface64a10
Mark Bradley <mbradle15@gmail.com>
2025-11-05 17:50:44 -0600

    added my contact information to the Contact Page

--- Grocery-Price-Comparison/src/components/pages/ContactPage.jsx
+++ Grocery-Price-Comparison/src/components/pages/ContactPage.jsx
@@ -5,6 +5,15 @@ const ContactPage = () => {
     return (
         <>
             <div>
+                <h1>Contact Information Page</h1>
+                <h2>My Contact Information</h2>
+                <address>
+                    <a href="mailto:email@example.com">email@example.com</a><br />
+                    <a href="tel:+13145551234">(314) 555-1234</a><br />
+                    Snail Mail to:<br />
+                    1234 Broadway Ave<br />
+                    St. Louis, MO 63111
+                </address>
                 <Form />
             </div>
         </>
