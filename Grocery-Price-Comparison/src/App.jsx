e77dc69e0277f300e80d1d35497dbb7a63019a43
Mark Bradley <mbradle15@gmail.com>
2025-11-05 18:46:30 -0600

    cleaning up my app.jsx file to get links to work

--- Grocery-Price-Comparison/src/App.jsx
+++ Grocery-Price-Comparison/src/App.jsx
@@ -1,31 +1,28 @@
-import { useState } from 'react'
-// import { BrowserRouter as Router, Routes, Route } from 'react-router'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import './App.css'
-import Header from './components/layout/Header'
-import Footer from './components/layout/Footer'
-// import AboutPage from './components/pages/AboutPage'
-import ContactPage from './components/pages/ContactPage'
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import AboutPage from './components/pages/AboutPage'
+import ContactPage from "./components/pages/ContactPage";
 // import ComparisonPage from './components/pages/ComparisonPage'
 // import CartPage from './components/pages/CartPage'
-// import HomePage from './components/pages/HomePage'
+import HomePage from "./components/pages/HomePage";
 
 function App() {
-
   return (
     <>
-     <Header />
-     <Router>
-       <Routes>
-        <Route path="/" element={<HomePage />} />
-        {/* <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/comparison" element={<ComparisonPage />} /> */}
-     </Routes>
-     </Router>
-     <Footer />
+      <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          {/* <Route path="/cart" element={<CartPage />} /> */}
+          {/* <Route path="/comparison" element={<ComparisonPage />} /> */}
+        </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
