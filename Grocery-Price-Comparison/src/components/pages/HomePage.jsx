e2f9b98fd918747968a3c726a24cd95d2a9e0741
Mark Bradley <mbradle15@gmail.com>
2025-11-20 15:35:56 -0600

    updated

--- Grocery-Price-Comparison/src/components/pages/HomePage.jsx
+++ Grocery-Price-Comparison/src/components/pages/HomePage.jsx
@@ -4,7 +4,7 @@ const HomePage = () => {
             <div className="main-content">
                 <h1 className="title">Welcome to the Grocery Price Comparison Tool!</h1>
                 <p>
-                    Have you ever looked at your grocery receipt and wondered "Am I paying to much?"
+                    Have you ever looked at your grocery receipt and wondered "Am I paying too much?"
                 </p>
                 <p>
                     If you have then this tool is for you. It takes all of the guesswork out of grocery shopping.
